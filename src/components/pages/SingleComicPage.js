3bbd6b1b4f1a003b232315ceb2da76db1e9a8248
Khozhabek <khozhabek1999@mail.ru>
2023-03-19 22:44:06 +0300

    add roouting to pages

---
+++ src/components/pages/SingleComicPage.js
@@ -0,0 +1,64 @@
+import './singleComicPage.scss';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import useMarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import { useParams } from 'react-router-dom';
+import AppBanner from '../appBanner/AppBanner';
+import Page404 from './Page404';
+
+const SingleComicPage = () => {
+  const { comicId } = useParams();
+  const [comic, setComic] = useState(null);
+  const { error, loading, getComic, clearError } = useMarvelService();
+
+  useEffect(() => {
+    updateComic();
+  }, [comicId]);
+
+  const updateComic = () => {
+    clearError();
+    getComic(comicId).then(onComicLoaded);
+  };
+
+  const onComicLoaded = (char) => {
+    setComic(char);
+  };
+
+  const errorMessage = error ? <Page404 /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error || !comic) ? <View comic={comic} /> : null;
+
+  return (
+    <>
+      <AppBanner />
+      {errorMessage}
+      {spinner}
+      {content}
+    </>
+  );
+};
+
+const View = ({ comic }) => {
+  const { title, description, pageCount, thumbnail, language, price } = comic;
+
+  return (
+    <div className="single-comic">
+      <img src={thumbnail} alt={title} className="single-comic__img" />
+      <div className="single-comic__info">
+        <h2 className="single-comic__name">{title}</h2>
+        <p className="single-comic__descr">{description}</p>
+        <p className="single-comic__descr">{pageCount}</p>
+        <p className="single-comic__descr">Language: {language}</p>
+        <div className="single-comic__price">{price}</div>
+      </div>
+      <Link to="/comics" className="single-comic__back">
+        Back to all
+      </Link>
+    </div>
+  );
+};
+
+export default SingleComicPage;
